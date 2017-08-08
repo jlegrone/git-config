34883c9d3f4fb6eb924e56684f9ab10bfedaf154
Jacob LeGrone <git@jacoblegrone.com>
2017-08-08 19:44:38 -0400

    refactor(git-validation): add git version to package.json engines field

--- scripts/validate-git-version.js
+++ scripts/validate-git-version.js
@@ -1,23 +1,19 @@
 const validateGitVersion = require('validate-git-version')
 const { blue, yellow, red, green, reset } = require('chalk')
-const { name: moduleName } = require('../package')
-
-const minimumVersion = '2.6.0'
+const { name, engines } = require('../package')
 
 const onInValid = (currentVersion) => console.log(yellow(
-`
-⚠️  Git version ${red(currentVersion)} is not compatible with ${moduleName}.
-   Please upgrade to git ${green(minimumVersion)} or later.
+`⚠️  Git version ${red(currentVersion)} is not compatible with ${name}.
+   Please upgrade to git ${green(engines.git)}.
 `
 ))
 
 const onValid = () => console.log(blue(
-`
-🎉  ${moduleName} install successful!
+`🎉  ${name} install successful!
 
    Type ${yellow('git aliases')} to view available aliases.
    ${reset('More documentation is available at https://github.com/jlegrone/git-config.')}
 `
 ))
 
-validateGitVersion(`^${minimumVersion}`, onInValid, onValid)
+validateGitVersion(engines.git, onInValid, onValid)
